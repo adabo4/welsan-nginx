c3da4da05ca0d7db49e13d2b166ee537eaac555f
Adriana <adabo4@gmail.com>
2023-09-04 20:55:11 +0200

    build 6

--- welsan/src/components/Kurzy/KurzyHeadline.js
+++ welsan/src/components/Kurzy/KurzyHeadline.js
@@ -1,6 +1,6 @@
 import "./kurzy.css";
-import butterfly from '../Kurzy/butterfly.webp';
-import flower from "../Kurzy/flower.webp";
+// import butterfly from '../Kurzy/butterfly.webp';
+// import flower from "../Kurzy/flower.webp";
 import "./kurzyheadline.css"
 
 
@@ -9,14 +9,14 @@ export default function KurzyHeadline(){
     return(
         <>
         <div className="kurzy-headline">
-                    <img src={butterfly} alt="butterfly-img" className='butterfly-img' />
+                    <img src="./img/autumn.png" alt="butterfly-img" className='butterfly-img' />
                      <div>  {/*tuto bol div style inline-block */}
                         <div className="small-circle"></div>
-                        <h3>KURZY 2023 - Spring term</h3>
+                        <h3>KURZY 2023 - Autumn term</h3>
                         <hr  className="line" />
                         <hr  className="thin" /> </div>
 
-                    <img src={flower} alt="flower-img" className='flower-img' />
+                    <img src="./img/pumpkin.png" alt="flower-img" className='flower-img' />
 
                 </div>
         </>
--- welsan/src/components/SmallSlider/SmallSlider.js
+++ welsan/src/components/SmallSlider/SmallSlider.js
@@ -1,65 +1,65 @@
 
-import BtnSlider from "../Slider/BtnSlider";
-import dataSlider from "../Slider/dataSlider";
-import './smallslider.css';
-import { useEffect, useState, useRef } from 'react';
+// import BtnSlider from "../Slider/BtnSlider";
+// import dataSlider from "../Slider/dataSlider";
+// import './smallslider.css';
+// import { useEffect, useState, useRef } from 'react';
 
 
-export default function SmallSlider() {
+// export default function SmallSlider() {
 
-    const timerRef = useRef(null);
+//     const timerRef = useRef(null);
 
-    const [slideIndex, setSlideIndex] = useState(1);
+//     const [slideIndex, setSlideIndex] = useState(1);
 
-    const nextSlide = () => {
-        if (slideIndex !== dataSlider.length) {
-            setSlideIndex(slideIndex + 1)
-        }
-        else if (slideIndex === dataSlider.length) {
-            setSlideIndex(1)
-        }
+//     const nextSlide = () => {
+//         if (slideIndex !== dataSlider.length) {
+//             setSlideIndex(slideIndex + 1)
+//         }
+//         else if (slideIndex === dataSlider.length) {
+//             setSlideIndex(1)
+//         }
 
-    }
+//     }
 
-    const prevSlide = () => {
-        if (slideIndex !== 1) {
-            setSlideIndex(slideIndex - 1);
-        }
-        else if (slideIndex === 1) {
-            setSlideIndex(dataSlider.length)
-        }
+//     const prevSlide = () => {
+//         if (slideIndex !== 1) {
+//             setSlideIndex(slideIndex - 1);
+//         }
+//         else if (slideIndex === 1) {
+//             setSlideIndex(dataSlider.length)
+//         }
 
-    }
+//     }
 
-    useEffect(() => {
-        if (timerRef.current) {
-            clearTimeout(timerRef.current);
-        }
-        timerRef.current = setTimeout(() => {
-            nextSlide();
-        }, 4000);
+//     useEffect(() => {
+//         if (timerRef.current) {
+//             clearTimeout(timerRef.current);
+//         }
+//         timerRef.current = setTimeout(() => {
+//             nextSlide();
+//         }, 4000);
 
-        return () => clearTimeout(timerRef.current);
-    }, [nextSlide]);
+//         return () => clearTimeout(timerRef.current);
+//     }, [nextSlide]);
 
-    return (
-        <>
-            <div className="small-container">
-                <div className="container-slider-small">
-                    {dataSlider.map((obj, index) => {
-                        return (
-                            <div
-                                key={obj.id}
-                                className={slideIndex === index + 1 ? "slide active-anim" : "slide"}>
-                                <img src={process.env.PUBLIC_URL + `/Imgs/img${index + 1}.webp`}></img>
-                            </div>
-                        )
-                    })
-                    }
-                    <BtnSlider moveSlide={nextSlide} direction={"next"} />
-                    <BtnSlider moveSlide={prevSlide} direction={"prev"} />
-                </div>
-            </div>
-       </>
-    )
-}
+//     return (
+//         <>
+//             <div className="small-container">
+//                 <div className="container-slider-small">
+//                     {dataSlider.map((obj, index) => {
+//                         return (
+//                             <div
+//                                 key={obj.id}
+//                                 className={slideIndex === index + 1 ? "slide active-anim" : "slide"}>
+//                                 <img src={process.env.PUBLIC_URL + `/Imgs/img${index + 1}.webp`}></img>
+//                             </div>
+//                         )
+//                     })
+//                     }
+//                     <BtnSlider moveSlide={nextSlide} direction={"next"} />
+//                     <BtnSlider moveSlide={prevSlide} direction={"prev"} />
+//                 </div>
+//             </div>
+//        </>
+//     )
+// }
