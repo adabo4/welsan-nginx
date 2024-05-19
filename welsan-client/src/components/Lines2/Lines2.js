47e6c11426ec7d74f51b27c36facd6e20a2345e2
adabo4 <130857407+adabo4@users.noreply.github.com>
2024-05-19 21:30:56 +0200

    added useCallBack in Slider

--- welsan-client/src/components/Lines2/Lines2.js
+++ welsan-client/src/components/Lines2/Lines2.js
@@ -3,27 +3,13 @@ import React from 'react'
 import "./lines2.css"
 
 const Lines2 = () => {
-
-  const style =
-    {
-    margin: "5px auto",
-    alignSelf: "center",
-    maxWidth: "75%",
-
-    }
-
-
-
   return (
     <>
-    
-    <div className="line-container-2">
-        <hr className="line-2"/>
-        <hr className="thin-2"/>
+      <div className="line-container-2">
+        <hr className="line-2" />
+        <hr className="thin-2" />
         <hr className="line-dotted-2" />
-    </div>
-        
-      
+      </div>
     </>
   )
 }
--- welsan-client/src/components/MediaQueryLines/MediaQueryLines2.js
+++ welsan-client/src/components/MediaQueryLines/MediaQueryLines2.js
@@ -1,16 +1,12 @@
-import Media from 'react-media';
-import Lines from '../Lines/Lines';
 import useMediaQuery from './useMediaQuery';
 import Lines2 from '../Lines2/Lines2';
 
-function MediaQueryLines2(){
-
-
+function MediaQueryLines2() {
     const matches = useMediaQuery("(max-width: 1110px)");
-    
-    return(
+
+    return (
         <>
-        {matches ? <Lines2></Lines2> : '' }
+            {matches ? <Lines2></Lines2> : ''}
         </>
     )
 }
