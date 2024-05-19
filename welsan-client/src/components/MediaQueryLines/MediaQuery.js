ee3aa65579f7d7ccef720adc5dbabd6293a9038e
adabo4 <130857407+adabo4@users.noreply.github.com>
2024-05-19 21:30:56 +0200

    added useCallBack in Slider

--- welsan-client/src/components/MediaQueryLines/MediaQuery.js
+++ welsan-client/src/components/MediaQueryLines/MediaQuery.js
@@ -1,15 +1,11 @@
-import Media from 'react-media';
 import Lines from '../Lines/Lines';
 import useMediaQuery from './useMediaQuery';
 
-function MediaQuery(){
-
-
+function MediaQuery() {
     const matches = useMediaQuery("(max-width: 1110px)");
-    
-    return(
+    return (
         <>
-        {matches ? '' : <Lines></Lines>}
+            {matches ? '' : <Lines></Lines>}
         </>
     )
 }
--- welsan-client/src/components/Slider/Slider2.js
+++ welsan-client/src/components/Slider/Slider2.js
@@ -1,128 +1,83 @@
 import BtnSlider from "./BtnSlider";
 import dataSlider from "./dataSlider";
 import './slider.css';
-import { useEffect, useState, useRef } from 'react';
-import Lines from '../Lines/Lines';
+import { useEffect, useState, useRef, useCallback } from 'react';
 import MediaQuery from "../MediaQueryLines/MediaQuery";
-import Lines2 from "../Lines2/Lines2";
 import MediaQueryLines2 from "../MediaQueryLines/MediaQueryLines2";
-
-
 
 function Slider2() {
     const timerRef = useRef(null);
 
     const [slideIndex, setSlideIndex] = useState(1);
 
-    const nextSlide = () =>{
-        if(slideIndex !== dataSlider.length){
-            setSlideIndex(slideIndex + 1)
-        } 
-        else if (slideIndex === dataSlider.length){
-            setSlideIndex(1)
+    const nextSlide = useCallback(() => {
+        if (slideIndex !== dataSlider.length) {
+            setSlideIndex(slideIndex + 1);
+        } else if (slideIndex === dataSlider.length) {
+            setSlideIndex(1);
         }
+    }, [slideIndex]);
 
-    }
+    useEffect(() => {
+        if (timerRef.current) {
+            clearTimeout(timerRef.current);
+        }
+        timerRef.current = setTimeout(() => {
+            nextSlide();
+        }, 4000);
 
-    const prevSlide = ()=>{
-        if(slideIndex !== 1) {
+        return () => clearTimeout(timerRef.current);
+    }, [nextSlide]);
+
+    const prevSlide = () => {
+        if (slideIndex !== 1) {
             setSlideIndex(slideIndex - 1);
         }
-        else if(slideIndex === 1) {
+        else if (slideIndex === 1) {
             setSlideIndex(dataSlider.length)
         }
 
     }
 
-    useEffect(() => {
-        if (timerRef.current) {
-          clearTimeout(timerRef.current);
-        }
-        timerRef.current = setTimeout(() => {
-          nextSlide();
-        }, 4000);
-    
-        return () => clearTimeout(timerRef.current);
-      }, [nextSlide]);
+    return (
+        <>
+            <div className="big-container">
+                <MediaQuery />
+                <div className="container-slider">
+                    {dataSlider.map((obj, index) => {
+                        return (
+                            <div
+                                key={obj.id}
+                                className={slideIndex === index + 1 ? "slide active-anim" : "slide"}>
+                                <img src={process.env.PUBLIC_URL + `/Imgs/img${index + 1}.webp`} alt="Slides"></img>
+                            </div>
 
-      
-    return(
-        <>    
-        
-        <div className="big-container">
-            {/* <div className="eng-bubble">
-        
-     <div className="eng-bubble-text">
-         <hr style={{width: 300 } } className="first-line"/>
-         <hr style={{width: 300 } } className="second-line" />
-         <h1>Angličtina v Trnave</h1>
-         <p>Nauč sa hovoriť a myslieť po anglicky!</p>
-         <hr style={{width: 300 } }  className="bubble-dotted"/>
-         <p>Pre úplných začiatočníkov i večných študentov</p>
-         <p>Deti, dospelí, firemné kurzy.</p>
-     </div>
-     
-     </div> */}
-         <MediaQuery />  
-        <div className="container-slider">
-    {dataSlider.map((obj, index) => {
-           return (
-            <div 
-            key={obj.id}
-            className = {slideIndex === index + 1 ? "slide active-anim" : "slide"}>
-            <img src={process.env.PUBLIC_URL + `/Imgs/img${index + 1}.webp`}></img>
+                        )
+                    })
+                    }
+                    <BtnSlider moveSlide={nextSlide} direction={"next"} />
+                    <BtnSlider moveSlide={prevSlide} direction={"prev"} />
+
+                </div>
+
+                {/* <Lines></Lines> tuto pojdu lines ked sa zmeni viewport a flexbox column */}
+                <MediaQueryLines2></MediaQueryLines2>
+
+                <div className="eng-bubble">
+
+                    <div className="eng-bubble-text">
+                        <hr className="first-line" />
+                        <hr className="second-line" />
+                        <h1>Angličtina v Trnave</h1>
+                        <p>Nauč sa hovoriť a myslieť po anglicky!</p>
+                        <hr className="bubble-dotted" />
+                        <p>Pre úplných začiatočníkov i večných študentov</p>
+                        <p>Deti, dospelí, firemné kurzy.</p>
+                    </div>
+
+                </div>
             </div>
-                
-            )
-        })
-    }
-    <BtnSlider moveSlide={nextSlide} direction={"next"} />
-    <BtnSlider moveSlide={prevSlide} direction={"prev"}/>
-    {/* {setTimeout(nextSlide, 4000)}; */}
-
-    
-
-    
-
-    
-       
-  
-    </div>
-   
-    {/* <div className="line-container">
-    <hr className="line"/>
-      <hr className="thin" />
-      <hr className="line-dotted"/>
-
-    </div> */}
-
-    {/* <Lines></Lines> tuto pojdu lines ked sa zmeni viewport a flexbox column */}
-    <MediaQueryLines2></MediaQueryLines2>
-    
-    
-    <div className="eng-bubble">
-        
-     <div className="eng-bubble-text">
-         <hr className="first-line"/>
-         <hr  className="second-line" />
-         <h1>Angličtina v Trnave</h1>
-         <p>Nauč sa hovoriť a myslieť po anglicky!</p>
-         <hr className="bubble-dotted"/>
-         <p>Pre úplných začiatočníkov i večných študentov</p>
-         <p>Deti, dospelí, firemné kurzy.</p>
-     </div>
-     
-     </div>
-     </div>
-    
-    
-    
-
-
-    
-   
-     
-     </>
+        </>
     )
 }
 
