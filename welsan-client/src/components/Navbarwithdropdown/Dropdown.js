5b10f659709c87c48dee64403be42954e11ce724
adabo4 <130857407+adabo4@users.noreply.github.com>
2024-05-19 21:56:36 +0200

    removed unused componens in Navbarwithdropdown

--- welsan-client/src/components/Navbarwithdropdown/Dropdown.js
+++ welsan-client/src/components/Navbarwithdropdown/Dropdown.js
@@ -1,42 +1,28 @@
 import { useState } from "react"
 import "./Dropdown.css";
-import { Link, NavLink } from 'react-router-dom';
-import { MenuItems } from "./MenuItems.js"    
+import { NavLink } from 'react-router-dom';
+import { MenuItems } from "./MenuItems.js"
 
 
-export default function Dropdown(){
-    
+export default function Dropdown() {
+
     const [click, setClick] = useState(false);
-
     const handleClick = () => setClick(!click);
 
-    
-
-
-
-
-    return(
+    return (
         <>
-        
-         <ul onClick={handleClick} className={click ? "dropdown-menu clicked" : 'dropdown-menu'}>
-    
-            {MenuItems.map((item,index)=>{
-                return(
-                        
-                    <li key={index} >
-                        <NavLink className={({isActive}) => isActive ? 'home dropdown-link' : 'dropdown-link' } to={item.path} onClick={() => setClick(false)}>
-                        
-                        {item.title}</NavLink></li>
-                
 
-                
-                )
-            })}
-            
+            <ul onClick={handleClick} className={click ? "dropdown-menu clicked" : 'dropdown-menu'}>
 
+                {MenuItems.map((item, index) => {
+                    return (
 
-         </ul>
-        
+                        <li key={index} >
+                            <NavLink className={({ isActive }) => isActive ? 'home dropdown-link' : 'dropdown-link'} to={item.path} onClick={() => setClick(false)}>
+                                {item.title}</NavLink></li>
+                    )
+                })}
+            </ul>
         </>
     )
 }
