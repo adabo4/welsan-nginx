cc7efc67d81ad831aea8f20d985f95c2983e2277
adabo4 <130857407+adabo4@users.noreply.github.com>
2024-09-29 23:14:22 +0200

    changed port

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ app.get('/', (req, res) => {
   res.send("API WORKING")
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
