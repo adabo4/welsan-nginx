a1b229329d7ef12ba9c60420a4a9243a47b3c447
adabo4 <130857407+adabo4@users.noreply.github.com>
2024-09-29 21:46:38 +0200

    deploy

---
+++ .scripts/deploy.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Navigate to the frontend directory
+set -e
+
+echo "Deployment started" 
+
+git pull origin master 
+echo "New changes copied to server!" 
+
+cd server
+
+echo "Installing dependencies"
+npm install --yes
+
+cd ../welsan
+npm install --yes
+
+echo "Running the build"
+npm run build
+
+# Clear the existing files in the destination directory
+rm -rf /var/www/welsan/*
+
+# Copy new build files to the destination directory
+cp -r build /var/www/welsan/
+
+pm2 reload server
