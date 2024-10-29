c5efab40a893ad70a6fa4a077111b8dbf16cee85
adabo4 <130857407+adabo4@users.noreply.github.com>
2024-10-29 20:08:59 +0100

    updated deploy file

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -1,58 +1,3 @@
-# #!/bin/bash
-# # Navigate to the frontend directory
-# set -e
-
-# echo "Deployment started" 
-
-# cd /var/www/git
-
-# git pull origin master
-# echo "New changes copied to server!" 
-
-# # Create .env for the server
-# echo "REACT_APP_CONVERTKIT_KEY=${REACT_APP_CONVERTKIT_KEY}" >> server/.env
-# echo "REACT_APP_CONVERTKIT_ID=${REACT_APP_CONVERTKIT_ID}" >> server/.env
-# # echo "REACT_APP_API_URL=${REACT_APP_API_URL}" >> server/.env
-
-# # # Create .env for the welsan
-# # echo "REACT_APP_CONVERTKIT_KEY=${REACT_APP_CONVERTKIT_KEY}" >> welsan/.env
-# # echo "REACT_APP_CONVERTKIT_ID=${REACT_APP_CONVERTKIT_ID}" >> welsan/.env
-# # echo "REACT_APP_API_URL=${REACT_APP_API_URL}" >> welsan/.env
-
-# cd server
-
-# echo "Installing dependencies"
-# npm install --yes
-
-# cd ../welsan
-# npm install --yes
-
-# echo "Running the build"
-# npm run build
-
-# # Create a temporary directory for the new build
-# TEMP_DIR="/var/www/welsan_temp"
-
-# # Remove the temporary directory if it exists
-# rm -rf $TEMP_DIR
-# mkdir $TEMP_DIR
-
-# # Copy new build files to the temporary directory
-# cp -r build/* $TEMP_DIR/
-
-# mv $TEMP_DIR/* /var/www/welsan/
-
-# # Remove the temporary directory
-# rmdir $TEMP_DIR
-
-# # # Clear the existing files in the destination directory
-# # rm -rf /var/www/welsan/*
-
-# # # Copy new build files to the destination directory
-# # cp -r build /var/www/welsan/
-
-# pm2 reload server2
-
 
 #!/bin/bash
 # Exit immediately if a command exits with a non-zero status
@@ -94,10 +39,6 @@ mkdir $TEMP_DIR
 
 # Copy new build files to the temporary directory
 cp -r build $TEMP_DIR/
-
-# # Ensure the production build directory exists
-# BUILD_DIR="/var/www/welsan/build"
-# mkdir -p $BUILD_DIR  # Create the directory if it doesn't exist
 
 # Move the new build files into the production directory
 mv $TEMP_DIR/build "/var/www/welsan/"
